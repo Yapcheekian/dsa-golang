ea16f6c9ebfb55f4eb8e025353073cb3eb234253
Gerard Yap <cheekian.yap@aiesec.net>
2022-01-07 22:30:53 +0800

    practice stack application

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,6 +1,9 @@
 package stack
 
 import (
+	"bufio"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,4 +29,36 @@ func TestStack(t *testing.T) {
 	result = sArray.Pop()
 	assert.Equal(t, "c", result)
 	assert.Equal(t, false, sArray.IsEmpty())
+}
+
+func TestArithmeticExpressionEvaluation(t *testing.T) {
+	vals := NewStackLinkedList()
+	ops := NewStackLinkedList()
+	f, _ := os.Open("arithmetic.txt")
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanRunes)
+	for s.Scan() {
+		text := s.Text()
+		switch text {
+		// ignore left parenthesis
+		case "(":
+		case "+":
+			ops.Push(text)
+		case "*":
+			ops.Push(text)
+		case ")":
+			num1, _ := strconv.Atoi(vals.Pop())
+			num2, _ := strconv.Atoi(vals.Pop())
+			op := ops.Pop()
+			if op == "+" {
+				vals.Push(strconv.Itoa(num1 + num2))
+			} else if op == "*" {
+				vals.Push(strconv.Itoa(num1 * num2))
+			}
+		default:
+			vals.Push(text)
+		}
+	}
+	assert.Equal(t, "101", vals.Pop())
+
 }
